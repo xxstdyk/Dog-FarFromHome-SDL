5526d5b47bfc86fc86cb390d7ea0cae0b731aeff
Hasan <hasanyousaf256@gmail.com>
2021-03-15 23:34:33 -0400

    Finished UI new
    finished UI with new background / resolution

--- src/StartScene.cpp
+++ src/StartScene.cpp
@@ -45,15 +45,15 @@ void StartScene::Start() {
 	//Background
 	DisplayObject *m_pBackground = new Background();
 	AddChild(m_pBackground);
-	const SDL_Color blue = { 0, 0, 255, 255 };
+	//const SDL_Color blue = { 0, 0, 255, 255 };
 	const SDL_Color white = { 255, 255, 255, 0 };
-	m_pStartLabel = new Label("Dog: Far From Home", "Consolas", 50, white, glm::vec2(550.0f, 150.0f));
+	m_pStartLabel = new Label("Dog: Far From Home", "Consolas", 95, white, glm::vec2(800.0f, 150.0f));
 	m_pStartLabel->SetParent(this);
 	AddChild(m_pStartLabel);
 
 	// StartButton
 	m_pStartButton = new Button();
-	m_pStartButton->GetTransform()->position = glm::vec2(550.0f, 350.0f);
+	m_pStartButton->GetTransform()->position = glm::vec2(800.0f, 425.0f);
 
 	m_pStartButton->AddEventListener(CLICK, [&]()-> void {
 		m_pStartButton->setActive(false);
@@ -71,7 +71,7 @@ void StartScene::Start() {
 
 	// Settings Button 
 	m_settingButton = new Button("../Assets/textures/settingsButton.png","settingsButton",SETTINGS_BUTTON);
-	m_settingButton->GetTransform()->position = glm::vec2(550.0f, 440.0f);
+	m_settingButton->GetTransform()->position = glm::vec2(800.0f, 525.0f);
 
 	m_settingButton->AddEventListener(CLICK, [&]()-> void {
 		m_settingButton->setActive(false);
