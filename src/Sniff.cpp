fd4d6117050efbfed232184e0785b6c28a1393b0
Dat <Davidvuong67@gmail.com>
2021-04-20 03:42:16 -0400

    I hate veryrthing
    new sniff sprite image intgeiterg

--- src/Sniff.cpp
+++ src/Sniff.cpp
@@ -2,7 +2,7 @@
 
 Sniff::Sniff() {
 	SetEnabled(false);
-	TextureManager::Instance()->load("../Assets/textures/GOODSHIT.png", "dogSniff");
+	TextureManager::Instance()->load("../Assets/textures/dogSniffImage.png", "dogSniff");
 }
 Sniff::~Sniff() {
 
@@ -10,8 +10,7 @@ Sniff::~Sniff() {
 
 // Inherited via GameObject
 void Sniff::Draw(){
-	TextureManager::Instance()->draw("dogSniff",
-		GetTransform()->position.x, GetTransform()->position.y);
+	TextureManager::Instance()->draw("dogSniff", GetTransform()->position.x, GetTransform()->position.y);
 }
 
 void Sniff::Update(){
